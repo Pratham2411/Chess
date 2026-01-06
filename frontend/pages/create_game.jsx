276d55a39c496f0e12acdf355936bd27bb52b10e
Pratham Raj <prathamraj2411@gmail.com>
2026-01-06 22:02:01 +0530

    Add files via upload

---
+++ frontend/pages/create_game.jsx
@@ -0,0 +1,90 @@
+import {useState, useEffect} from "react"
+import configData from "../config.json"
+import Link from 'next/link'
+
+import io from "socket.io-client"
+let socket = io.connect(configData.SOCKET_URL)
+
+
+
+function createGame(){
+    const [username, setUsername] = useState("");
+    const [friendUsername, setFriendUsername] = useState("");
+    const [side, setSide] = useState("white")
+
+    useEffect(()=>{
+        console.log(session);
+    }, [])
+
+    function connectToGame(){
+
+        socket.emit("connect-to-game", {
+            username: username,
+            friendUsername: friendUsername
+        });
+
+    }
+    function createGame(){
+        socket.emit("create-user", {username: username, side: side});
+
+    }
+
+    return(
+        <div>
+            <label htmlFor="userName">Your preffered username: </label>
+            <input 
+                value={username} 
+                type="text" 
+                name="userName" 
+                placeholder="Wonton Soup"
+                onChange={e => {setUsername(e.target.value)}}
+            />
+            <br />
+            <br />
+            <label>
+                <input  type="radio" 
+                        checked={side==="white"}
+                        onChange={() =>{setSide("white")}} 
+                        name="side"/>
+                White
+            </label>
+            <br />
+            <label>
+                <input  type="radio" 
+                        checked={side==="black"}
+                        onChange={() =>{setSide("black")}}
+                        name="side"/>
+
+                Black
+            </label>
+            <br />
+            <br />
+            <Link href="/chess">
+                <button onClick={createGame} > Create A Game </button>
+            </Link>
+
+            <br />
+            <br />
+            <br />
+            <label htmlFor="friendName">Your friend already set up a game?? Join their game by their username</label>
+            <br />
+            <br />
+            <input 
+                value={friendUsername} 
+                type="text" 
+                name="friendName" 
+                placeholder="Corn Soup"
+                onChange={e => {setFriendUsername(e.target.value)}}
+            />
+            <Link href="/chess">
+                <button onClick={connectToGame} > Join Game </button>
+            </Link>
+
+
+        </div>
+    )
+
+}
+
+
+export default createGame;
---
+++ server/server.py
@@ -0,0 +1,102 @@
+from flask import Flask, redirect, url_for, request, session
+from flask_session import Session
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from config import ApplicationConfig
+
+
+app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
+
+socketio = SocketIO(app, cors_allowed_origins='*')
+server_session = Session(app)
+
+#Set-up logging to ERROR only
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+#Change on deployment
+app.host = 'localhost'
+app.debug = True;
+USERS={}
+#------------------------- socket ----------------------------------------
+@socketio.on('connect')
+def connect(auth):
+    pass
+    # print("----------------------- | Connected | -----------------------------")
+    # print(f"User Connected with ID: {request.sid}")
+    # print("-------------------------------------------------------------")
+
+@socketio.on('connect-to-game')
+def connect_to_game(payload):
+    try:
+        roomHost = payload['friendUsername']
+        userName = payload['username']
+        USERS[roomHost]["with"] = userName
+        USERS[roomHost]["withSid"] = request.sid
+        USERS[roomHost]["status"] = "in-game"
+
+        # Create the player object
+        USERS[userName] = {
+            "status" : "in-game",
+            "sid"    : request.sid,
+            "with"   : roomHost,
+            "withSid": USERS[roomHost]["sid"],
+        }
+        if(USERS[roomHost]["side"] == "black"):
+            USERS[userName]["side"] = "white"
+        else:
+            USERS[userName]["side"] = "black"
+        return True
+    except:
+        return False
+
+
+@socketio.on("create-user")
+def onCreateUser(payload):
+    USERS[payload['username']] = {
+        "status" : "pending",
+        "sid"    : request.sid,
+        "with"   : "None",
+        "withSid": "None",
+        "side"   : payload['side']
+    }
+    session['username']= payload['username']
+
+@socketio.on('disconnect')
+def disconnect():
+    # print("########################| Disconnected | ########################")
+    # print(f"User disconnected with ID: {request.sid}")
+    # print("##################################################################")
+    pass
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+    return None
+
+@socketio.on("chess-move")
+def handleChessMove(chessMoveData):
+    emit("update-chess-move", chessMoveData, broadcast=True)
+    print(f"{request.sid} Move: {chessMoveData}")
+
+#------------------------- app routing ------------------------------------
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<div> Hello </div>"
+
+@app.route('/user', methods=['GET'])
+def get_user():
+    return USERS
+
+if __name__ == "__main__":
+    socketio.run(app, port=5000)
